758aa777ad52881e68a48700d8fe634b70739e6d
Justin Zaager <hextechprotect@gmail.com>
2022-09-29 22:34:11 -0400

    Set modal to open on book click and display info

--- script.js
+++ script.js
@@ -1,36 +1,17 @@
-/* 
-  ***!!!LOOK UP HOW TO USE FORM DATA ONCE SUBMITTED!!!
-
-  Create Book constructor to be called whenever creating a new book
-
-  Create a function that will add a book to the library array
-
-  Create a function that loops through the array and displays
-  each book on the page
-
-  Add an event listener to each book to display its info on click
-  (maybe the info is hidden and the display is toggled on click?)
-
-  Add a button on each book's display to remove it from the library
-    - see data-attribute
-
-  Add a button on each book's display to change its read status
-    - create a helper function that toggles a book's read status on 
-      the Book prototype
- */
-
 const myLibrary = [];
 
 const bookshelf = document.querySelector('.bookshelf');
 const addBookButton = document.querySelector('[type="submit"]');
 const form = document.querySelector('form');
+const modal = document.querySelector('.modal');
+const closeButton = document.querySelector('.close-button');
+const removeButton = document.querySelector('.remove-button');
+
 const inputTitle = document.querySelector('#book-title');
 const inputAuthor = document.querySelector('#author');
 const inputPageCount = document.querySelector('#page-numbers');
 const inputReadStatus = document.querySelector('#read-complete');
-const modal = document.querySelector('.modal');
-const closeButton = document.querySelector('.close-button');
-const removeButton = document.querySelector('.remove-button');
+
 
 function Book(title, author, pageCount, readStatus) {
   this.title = title,
@@ -39,25 +20,51 @@ function Book(title, author, pageCount, readStatus) {
   this.haveRead = readStatus
 }
 
+Book.prototype.getIndex = function() {
+  return myLibrary.indexOf(this);
+}
 Book.prototype.setIndex = function() {
   return this.index = myLibrary.indexOf(this);
 } 
 
-// Create new book and add to myLibrary array
+// Create new book, add to myLibrary array, set it's index
 function addBookToLibrary() {
   const newBook = new Book(inputTitle.value, inputAuthor.value,
-        inputPageCount.value, inputReadStatus.checked);
+    inputPageCount.value, inputReadStatus.checked);
+
   myLibrary.push(newBook);
-  console.log(newBook.setIndex());
+  newBook.setIndex();
+  displayAllBooks();
 }
 
-function getBookValues() {
+// Display books in myLibrary array on the shelf visually
+function displayAllBooks() {
+  const bookOnShelf = document.createElement('div');
 
+  for (let i = 0; i < myLibrary.length; i++) {
+    bookOnShelf.classList.add('book');
+    bookOnShelf.classList.add('created-book')
+    bookOnShelf.style.backgroundColor = randomBgColor();
+    bookOnShelf.setAttribute('index', `${i}`);
+    // Callback function here to prevent multiple callbacks on each click
+    bookOnShelf.addEventListener('click', showModalInfo);
+  }
+  bookshelf.append(bookOnShelf);
 }
 
-// Display a book on the bookshelf for each book in myLibrary
-function addBooksToShelf() {
-
+function showModalInfo() {
+  // Get all visually created books from the DOM
+  const books = document.querySelectorAll('.created-book');
+  
+  /* Sets the modal's info for the clicked-on book
+   to the book from myLibrary with the corresponding index */
+  books.forEach( (book) => {
+    const bookIndex = book.getAttribute('index');
+    modalTitle.textContent = myLibrary[bookIndex].title;
+    modalAuthor.textContent = myLibrary[bookIndex].author;
+    modalPageCount.textContent = myLibrary[bookIndex].pageCount;
+  })
+  modal.showModal();
 }
 
 // Random color for each new book
@@ -76,9 +83,3 @@ form.addEventListener('submit', function handleSubmit(event) {
 
 closeButton.addEventListener('click', () => modal.close());
 addBookButton.addEventListener('click', addBookToLibrary);
-
-// ADD AN EVENT LISTENER TO EACH DISPLAYED BOOK
-  // ON CLICK --> open modal (dialog)
-  // DISPLAY THE BOOK'S INFO
-  // ADD BUTTON TO REMOVE BOOK
-  // ADD BUTTON TO CHANGE READ STATUS
