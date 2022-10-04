1fd5acaca8f1ba3f0c426094190260c1946cc1f5
Justin Zaager <hextechprotect@gmail.com>
2022-10-04 14:36:04 -0400

    Add function to create book from user input info

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-const myLibrary = [];
+let myLibrary = [];
 
 const form = document.querySelector('form');
+const addBookButton = document.querySelector('.submit-button');
 
+function Book(title, author, pageCount, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.pageCount = pageCount;
+  this.readStatus = readStatus;
+}
 
+function addBookToLibrary() {
+  const inputTitle = document.querySelector('#book-title').value;
+  const inputAuthor = document.querySelector('#author').value;
+  const inputPageCount = document.querySelector('#page-numbers').value;
+  const inputReadStatus = document.querySelector('#read-complete').checked;
+
+  const newBook = new Book(inputTitle, inputAuthor, inputPageCount, inputReadStatus);
+  return myLibrary.push(newBook);
+}
 
 // Event Listeners
 form.addEventListener('submit', function handleSubmit(event) {
   event.preventDefault();
-})
+  form.reset();
+});
+
+addBookButton.addEventListener('click', addBookToLibrary);
