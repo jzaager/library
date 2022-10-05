e9ec7254dad0fda9da1d134209b6f195fa6749c1
Justin Zaager <hextechprotect@gmail.com>
2022-10-04 21:56:42 -0400

    Add read and remove buttons for each display book

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let myLibrary = [];
 const bookshelf = document.querySelector('.bookshelf');
 const form = document.querySelector('form');
 const addBookButton = document.querySelector('.submit-button');
+const buttonReadCompleteColor = 'rgba(132, 251, 194, 1)';
+const buttonReadIncompleteColor = 'rgba(227, 227, 227, 1)';
 
 function Book(title, author, pageCount, readStatus) {
   this.title = title;
@@ -14,7 +16,6 @@ Book.prototype.getIndex = function() {
   return myLibrary.indexOf(this);
 }
 Book.prototype.displayAllBooks = function() {
-
   const newDisplayBook = document.createElement('div');
   const displayBookTitle = document.createElement('h2');
   const displayBookAuthor= document.createElement('h3');
@@ -23,21 +24,50 @@ Book.prototype.displayAllBooks = function() {
   const removeButton = document.createElement('button');
 
   for (let i = 0; i < myLibrary.length; i++) {
-  
+    
     newDisplayBook.classList.add('book', 'created-book');
-    newDisplayBook.setAttribute('index', this.getIndex());
+    newDisplayBook.setAttribute('book-index', this.getIndex());
+    removeButton.setAttribute('index', this.getIndex());
     
     displayBookTitle.textContent = this.title;
     displayBookAuthor.textContent = this.author;
     displayBookPageCount.textContent = this.pageCount;
 
-    readButton.classList.add('read-button');
     removeButton.classList.add('remove-button');
-    readButton.textContent = (this.readStatus === true) ? 'Read' : 'Not read';
     removeButton.textContent = 'Remove';
+    removeButton.addEventListener('click', deleteBook);
+
+    readButton.classList.add('read-button');
+    readButton.textContent = (this.readStatus === true) ? 'Read' : 'Not read';
+    readButton.style.backgroundColor = (this.readStatus === true) ? buttonReadCompleteColor : buttonReadIncompleteColor;
+    readButton.addEventListener('click', toggleReadStatus);
 
     newDisplayBook.append(displayBookTitle, displayBookAuthor,
       displayBookPageCount, readButton, removeButton);
+  }
+  function deleteBook() {
+    const index = this.getAttribute('index')
+    bookshelf.querySelector(`[book-index="${index}"`).remove();
+    myLibrary.splice([index], 1);
+
+    const domBooks = document.querySelectorAll('.created-book');
+    const domRemoveButtons = document.querySelectorAll('.remove-button');
+    for (let i = 0; i < myLibrary.length; i++) {
+      domBooks[i].setAttribute('book-index', i);
+      domRemoveButtons[i].setAttribute('index', i);
+
+    }
+  }
+  function toggleReadStatus() {
+    if (!(this.readStatus === true)) {
+      this.readStatus = true;
+      this.textContent = 'Read';
+      readButton.style.backgroundColor = buttonReadCompleteColor;
+    } else {
+      this.readStatus = false;
+      this.textContent = 'Not read';
+      readButton.style.backgroundColor = buttonReadIncompleteColor;
+    }
   }
   bookshelf.append(newDisplayBook);
 }
@@ -47,12 +77,16 @@ function addBookToLibrary() {
   const inputAuthor = document.querySelector('#author').value;
   const inputPageCount = document.querySelector('#page-numbers').value;
   const inputReadStatus = document.querySelector('#read-complete').checked;
-  const newBook = new Book(inputTitle, inputAuthor, `${inputPageCount} pages`, inputReadStatus);
+  const newBook = new Book(inputTitle, inputAuthor, inputPageCount, inputReadStatus);
  
   if (inputTitle) {
     myLibrary.push(newBook);
     newBook.displayAllBooks();
   }
+}
+
+function createDisplayBook() {
+
 }
 
 // Event Listeners
