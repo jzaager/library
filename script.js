3929b76753f59b94edc6dd76f57bc1804973794d
Justin Zaager <hextechprotect@gmail.com>
2022-10-05 08:54:54 -0400

    Create helper functions to create book's buttons
    The displayAllBooks function was long and messy - this helped to
    abstract away some of that mess.

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let myLibrary = [];
 const bookshelf = document.querySelector('.bookshelf');
 const form = document.querySelector('form');
 const addBookButton = document.querySelector('.submit-button');
-const buttonReadCompleteColor = 'rgba(132, 251, 194, 1)';
-const buttonReadIncompleteColor = 'rgba(227, 227, 227, 1)';
+const buttonReadCompleteColor = 'rgba(132, 251, 194, 0.7)';
+const buttonReadIncompleteColor = 'rgba(227, 227, 227, 0.7)';
 
 function Book(title, author, pageCount, readStatus) {
   this.title = title;
@@ -16,65 +16,79 @@ Book.prototype.getIndex = function() {
   return myLibrary.indexOf(this);
 }
 Book.prototype.displayAllBooks = function() {
+
   const newDisplayBook = document.createElement('div');
   const displayBookTitle = document.createElement('h2');
   const displayBookAuthor= document.createElement('h3');
   const displayBookPageCount = document.createElement('p');
-  const readButton = document.createElement('button');
-  const removeButton = document.createElement('button');
 
   for (let i = 0; i < myLibrary.length; i++) {
     
     newDisplayBook.classList.add('book', 'created-book');
     newDisplayBook.setAttribute('book-index', this.getIndex());
-    removeButton.setAttribute('index', this.getIndex());
-    readButton.setAttribute('index', this.getIndex());
     
     displayBookTitle.textContent = this.title;
     displayBookAuthor.textContent = this.author;
     displayBookPageCount.textContent = this.pageCount;
 
-    removeButton.classList.add('remove-button');
-    removeButton.textContent = 'Remove';
-    removeButton.addEventListener('click', deleteBook);
+    //            //
 
-    readButton.classList.add('read-button');
-    readButton.textContent = (this.readStatus === true) ? 'Read' : 'Not read';
-    readButton.style.backgroundColor = (this.readStatus === true) ? buttonReadCompleteColor : buttonReadIncompleteColor;
-
-    newDisplayBook.append(displayBookTitle, displayBookAuthor,
-      displayBookPageCount, readButton, removeButton);
+    
   }
-  
-  function deleteBook() {
-    const index = this.getAttribute('index')
-    bookshelf.querySelector(`[book-index="${index}"`).remove();
-    myLibrary.splice([index], 1);
+  const boundCreateRemoveButton = createRemoveButton.bind(newDisplayBook);
+  const boundCreateReadButton = createReadButton.bind(newDisplayBook);
 
-    const domBooks = document.querySelectorAll('.created-book');
-    const domRemoveButtons = document.querySelectorAll('.remove-button');
-    const domReadButtons = document.querySelectorAll('.read-button');
+  newDisplayBook.append(displayBookTitle, displayBookAuthor,
+    displayBookPageCount, boundCreateReadButton(), boundCreateRemoveButton());
 
-    for (let i = 0; i < myLibrary.length; i++) {
-      domBooks[i].setAttribute('book-index', i);
-      domRemoveButtons[i].setAttribute('index', i);
-      domReadButtons[i].setAttribute('index', i);
-    }
-  }
-  function toggleReadStatus(e) {
-    const bookTarget = e.target.getAttribute('index');
-    if (!(myLibrary[bookTarget].readStatus === true)) {
-      myLibrary[bookTarget].readStatus = true;
-      readButton.textContent = 'Read';
-      readButton.style.backgroundColor = buttonReadCompleteColor;
-    } else {
-      myLibrary[bookTarget].readStatus = false;
-      readButton.textContent = 'Not read';
-      readButton.style.backgroundColor = buttonReadIncompleteColor;
-    }
-  }
-  readButton.addEventListener('click', toggleReadStatus);
   bookshelf.append(newDisplayBook);
+}
+
+function createReadButton() {
+  const readButton = document.createElement('button');
+  const index = this.getAttribute('book-index');
+
+  readButton.classList.add('read-button');
+  readButton.textContent = (myLibrary[index].readStatus === true) ? 'Read' : 'Not read';
+  readButton.style.backgroundColor = (myLibrary[index].readStatus === true) ? buttonReadCompleteColor : buttonReadIncompleteColor;
+  readButton.addEventListener('click', toggleReadStatus);
+
+  return readButton;
+}
+
+function createRemoveButton() {
+  const removeButton = document.createElement('button');
+
+  removeButton.classList.add('remove-button');
+  removeButton.textContent = 'Remove';
+  removeButton.addEventListener('click', deleteBook);
+
+  return removeButton;
+}
+
+function toggleReadStatus() {
+  const bookTarget = this.parentNode.getAttribute('book-index');
+
+  if (!(myLibrary[bookTarget].readStatus === true)) {
+    myLibrary[bookTarget].readStatus = true;
+    this.textContent = 'Read';
+    this.style.backgroundColor = buttonReadCompleteColor;
+  } else {
+    myLibrary[bookTarget].readStatus = false;
+    this.textContent = 'Not read';
+    this.style.backgroundColor = buttonReadIncompleteColor;
+  }
+}
+
+function deleteBook() {
+  const index = this.parentNode.getAttribute('book-index')
+  bookshelf.querySelector(`[book-index="${index}"`).remove();
+  myLibrary.splice([index], 1);
+
+  const domBooks = document.querySelectorAll('.created-book');
+  for (let i = 0; i < myLibrary.length; i++) {
+    domBooks[i].setAttribute('book-index', i);
+  }
 }
 
 function addBookToLibrary() {
